c05c6dad1d2aa5ef62e963cadc50f16c5226c74d
Tyler Hancock <darklime@live.ca>
2019-06-04 13:03:50 -0600

    Removed hoe and shear enchantment tweaks, and undead horse taming tweaks.

--- src/main/java/net/darkhax/nautilus/Nautilus.java
+++ src/main/java/net/darkhax/nautilus/Nautilus.java
@@ -7,9 +7,4 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = "nautilus", name = "Nautilus", version = "@VERSION@", certificateFingerprint = "@FINGERPRINT@")
 public class Nautilus {
     
-    @EventHandler
-    public void onPreInit (FMLPreInitializationEvent event) {
-        
-        new ConfigurationHandler(event.getSuggestedConfigurationFile());
-    }
 }
